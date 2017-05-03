f1d215e25eed65e32635eab4eb8ad7430eedb2e1
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-03 10:47:13 +0200

    Extends servlet and build war

--- simple-client/src/main/java/SimpleController.java
+++ simple-client/src/main/java/SimpleController.java
@@ -1,12 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
-public class SimpleController {
+public class SimpleController extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     @ResponseBody
