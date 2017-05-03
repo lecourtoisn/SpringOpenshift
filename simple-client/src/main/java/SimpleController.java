20dff43f66909ac5bb5adc8cdccd73d0e67b1a7e
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-03 11:32:23 +0200

    Added @SpringbootApplication

--- simple-client/src/main/java/SimpleController.java
+++ simple-client/src/main/java/SimpleController.java
@@ -1,5 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@SpringBootApplication
 public class SimpleController extends SpringBootServletInitializer {
 
     @RequestMapping("/")
