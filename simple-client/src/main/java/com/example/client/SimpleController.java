82a65599dc7e0c03b15434abadf7ea8a18cae0a9
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-11 10:36:17 +0200

    Restoring servlet dependencies and removing "@EnableAutoConfiguration", just to see

--- simple-client/src/main/java/com/example/client/SimpleController.java
+++ simple-client/src/main/java/com/example/client/SimpleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @SpringBootApplication
 public class SimpleController extends SpringBootServletInitializer {
 
