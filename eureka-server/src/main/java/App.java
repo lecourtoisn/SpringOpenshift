e36613123b2bf6ef90fd5ffa7c865cba5781b5bc
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-11 12:38:17 +0200

    Eureka Server

---
+++ eureka-server/src/main/java/App.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.web.support.SpringBootServletInitializer;
+        import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class App extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
