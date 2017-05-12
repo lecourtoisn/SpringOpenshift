9b56ebb54980bf020630ac2d1c2541f82f2bd556
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-12 15:24:29 +0200

    Add eureka client annotation

--- simple-client/src/main/java/com/example/client/SimpleController.java
+++ simple-client/src/main/java/com/example/client/SimpleController.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
+@EnableEurekaClient
 public class SimpleController extends SpringBootServletInitializer {
 
     @RequestMapping("/")
