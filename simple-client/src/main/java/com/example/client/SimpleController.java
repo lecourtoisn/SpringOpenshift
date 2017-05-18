e49b77085b81dae5104e7513542a933f3c3d6dfe
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-18 15:54:00 +0200

    A little bit of cleaning

--- simple-client/src/main/java/com/example/client/SimpleController.java
+++ simple-client/src/main/java/com/example/client/SimpleController.java
@@ -8,35 +8,27 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
 public class SimpleController extends SpringBootServletInitializer {
-//    @Value("${eureka_server}")
-//    String eurekaServer = "No eureka server found";
-
-    @Value("${welcome_message}")
-    String welcomeMessage = "No welcome message found";
+    @Value("${welcome_message:No welcome message found}")
+    String welcomeMessage = "";
 
     @Value("${not_existing_config:TheTest}")
-    String notExistingConfig = "No such thing";
+    String notExistingConfig = "";
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        String message = "";
-        RestTemplate template = new RestTemplate();
-        String result = template.getForObject("http://eureka-server.springopenshift.svc:8080/eureka/apps", String.class);
-        return result;
+        return "Hello world!";
     }
 
     @RequestMapping("hello")
     @ResponseBody
     String hello() {
         StringBuilder builder = new StringBuilder();
-//        builder.append(eurekaServer).append("\n");
         builder.append(welcomeMessage).append("\n");
         builder.append(notExistingConfig).append("\n");
         return builder.toString();
