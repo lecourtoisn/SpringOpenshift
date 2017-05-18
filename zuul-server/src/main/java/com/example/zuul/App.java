7dc0977432bf091b9fa62490dd997b36ca9bc2a7
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-18 17:06:22 +0200

    Enable Zuul Proxy instead of Zuul Server

--- zuul-server/src/main/java/com/example/zuul/App.java
+++ zuul-server/src/main/java/com/example/zuul/App.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulServer
+@EnableZuulProxy
 public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
