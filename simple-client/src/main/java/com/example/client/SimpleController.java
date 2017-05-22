d4e3eb7cb211003f8c9103e68f3704d00d07c847
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-22 11:12:10 +0200

    Hello route show eureka info about Artemis instances

--- simple-client/src/main/java/com/example/client/SimpleController.java
+++ simple-client/src/main/java/com/example/client/SimpleController.java
@@ -1,17 +1,27 @@
 package com.example.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
 public class SimpleController extends SpringBootServletInitializer {
+
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -21,11 +31,18 @@ public class SimpleController extends SpringBootServletInitializer {
     @RequestMapping("hello")
     @ResponseBody
     String hello() {
-//        StringBuilder builder = new StringBuilder();
-//        builder.append(welcomeMessage).append("\n");
-//        builder.append(notExistingConfig).append("\n");
-//        return builder.toString();
-        return "Ok";
+        StringBuilder sb = new StringBuilder();
+        List<ServiceInstance> instances = this.discoveryClient.getInstances("Artemis");
+        for (ServiceInstance instance : instances) {
+            sb.append("Host: ").append(instance.getHost()).append("<br \\>");
+            sb.append("Port: ").append(instance.getPort()).append("<br \\>");
+            sb.append("ServiceId: ").append(instance.getServiceId()).append("<br \\>");
+            sb.append("Uri: ").append(instance.getUri()).append("<br \\>");
+            sb.append("Metadata: ").append(instance.getMetadata()).append("<br \\>");
+            sb.append("<br \\>");
+            sb.append("__________________________________");
+        }
+        return sb.toString();
     }
 
 
