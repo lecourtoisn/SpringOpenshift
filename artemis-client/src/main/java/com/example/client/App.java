fa8e3734645dea4d934d29f3790eeb39cfce0abc
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-24 15:44:27 +0200

    Using ribbon to contact beowulf from artemis

--- artemis-client/src/main/java/com/example/client/App.java
+++ artemis-client/src/main/java/com/example/client/App.java
@@ -6,10 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -18,11 +22,21 @@ import java.util.List;
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "Beowulf", configuration = BeowulfConfiguration.class)
 public class App extends SpringBootServletInitializer {
 
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @RequestMapping("/")
     @ResponseBody
@@ -35,6 +49,9 @@ public class App extends SpringBootServletInitializer {
             e.printStackTrace();
         }
         sb.append(hostname).append("<br \\>");
+        sb.append("<br \\>");
+        String beowulfStr = this.restTemplate.getForObject("http://Beowulf/", String.class);
+        sb.append("Message from Beowulf: ").append(beowulfStr).append("<br \\>");
         return sb.toString();
     }
 
