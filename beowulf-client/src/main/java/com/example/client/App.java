f7e62b154a41bcf198d945183e600d6c977e843d
Nicolas Lecourtois <lecourtoisn@gmail.com>
2017-05-24 16:01:48 +0200

    Identify bewolf pod

--- beowulf-client/src/main/java/com/example/client/App.java
+++ beowulf-client/src/main/java/com/example/client/App.java
@@ -1,21 +1,36 @@
 package com.example.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Controller
 @SpringBootApplication
 @EnableEurekaClient
 public class App extends SpringBootServletInitializer {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World! I'm Beowulf";
+        String hostname = "";
+        try {
+            hostname = InetAddress.getLocalHost().toString();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "Hello World! I'm Beowulf : " + hostname;
     }
 
     public static void main(String[] args) throws Exception {
